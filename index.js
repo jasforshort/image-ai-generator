be1e23f2929882fd2ff64e4bdf02aa6803023479
Jas Mann <jasforshort@hotmail.ca>
2022-12-12 17:03:08 -0500

    Setup server and basic route

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("123")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
