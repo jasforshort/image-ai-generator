2fd5507df0684788ebacbb29554616c19d9980ea
Jas Mann <jasforshort@hotmail.ca>
2022-12-12 17:20:53 -0500

    Take in body request and apply to parameters for generating an image

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,11 +6,16 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
+
+  const { prompt, size } = req.body;
+
+  const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
   try {
     const response = await openai.createImage({
-      prompt: 'Penguin skating on water',
+      prompt,
       n: 1,
-      size: '512x512'
+      size: imageSize
     });
 
     const imageURL = response.data.data[0].url;
@@ -26,7 +31,7 @@ const generateImage = async (req, res) => {
     } else {
       console.log(error.message);
     }
-    
+
     res.status(400).json({
       success: false,
       error: 'Image could not be generated'
