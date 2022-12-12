79eaf63388e47c07ac5eeeb503777bd7eb5566bf
Jas Mann <jasforshort@hotmail.ca>
2022-12-12 17:32:37 -0500

    Add basic html page and styling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +8,8 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
